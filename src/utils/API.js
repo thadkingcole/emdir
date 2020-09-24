137731b91090a3611a5c7b608570afdc980908c9
Thad Cole <thadjcole@gmail.com>
2020-09-23 20:27:46 -0400

    use seed 'thadkingcole' so that same list of random users is returned each time

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// seed to be used by random user API
+const seed = "thadkingcole";
+
 export default {
   getUsers: () => {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us")
-  }
-}
+    return axios.get(
+      `https://randomuser.me/api/?results=200&nat=us&seed=${seed}`
+    );
+  },
+};
